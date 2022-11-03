e8a65e896a8ef841206e31ef74e89bf8c7995471
YevhenSerdiuk2022 <99655492+YevhenSerdiuk2022@users.noreply.github.com>
2022-11-03 17:43:14 +0000

    Deploying to gh-pages from @ YevhenSerdiuk2022/goit-js-hw-10@5348aa3d68eec96d7472e697267874d4a29e0368 🚀

---
+++ 01-color-switcher.50db64a5.js
@@ -0,0 +1,2 @@
+!function(){let t=null;const n={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};n.startBtn.addEventListener("click",(function(){n.startBtn.disabled=!0,n.stopBtn.disabled=!1,t=setInterval((()=>{var t;t=`#${Math.floor(16777215*Math.random()).toString(16)}`,document.body.style.backgroundColor=t,console.log("запускаем цвет")}),1e3)})),n.stopBtn.addEventListener("click",(function(){clearInterval(t),n.startBtn.disabled=!1,n.stopBtn.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.50db64a5.js.map
